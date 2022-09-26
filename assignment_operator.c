6c2b5a644ee84615ee97a9c24c5680174b2e1169
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2022-09-26 22:58:42 +0600

    Create assignment_operator.c

---
+++ assignment_operator.c
@@ -0,0 +1,48 @@
+//---equal to---
+#include <stdio.h>
+int main()
+{
+ int num = 5;
+ printf("%d\n", num);
+}
+//---plus equal to---
+#include <stdio.h>
+int main()
+{
+ int num = 5;
+ num += 1;
+ printf("%d\n", num);
+}
+//---minus equal to---
+#include <stdio.h>
+int main()
+{
+ int num = 5;
+ num -= 1;
+ printf("%d\n", num);
+}
+//---multiply equal to---
+#include <stdio.h>
+int main()
+{
+ int num = 5;
+ num *= 2;
+ printf("%d\n", num);
+}
+//---divide equal to---
+#include <stdio.h>
+int main()
+{
+ int num = 8;
+ num /= 2;
+ printf("%d\n", num);
+}
+//---modulas equal to---
+#include <stdio.h>
+int main()
+{
+ int num = 8;
+ num %= 3;
+ //num = (num%2);
+ printf("%d\n", num);
+}
