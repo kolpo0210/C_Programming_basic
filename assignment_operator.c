96c3520a22a531a42b6f25dbc651221df69e4949
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2022-09-27 20:50:17 +0600

    Update assignment_operator.c

--- assignment_operator.c
+++ assignment_operator.c
@@ -1,48 +1,33 @@
-//---equal to---
+
 #include <stdio.h>
 int main()
 {
+ //---equal to---
  int num = 5;
  printf("%d\n", num);
-}
-//---plus equal to---
-#include <stdio.h>
-int main()
-{
- int num = 5;
- num += 1;
- printf("%d\n", num);
-}
-//---minus equal to---
-#include <stdio.h>
-int main()
-{
- int num = 5;
- num -= 1;
- printf("%d\n", num);
-}
-//---multiply equal to---
-#include <stdio.h>
-int main()
-{
- int num = 5;
- num *= 2;
- printf("%d\n", num);
-}
-//---divide equal to---
-#include <stdio.h>
-int main()
-{
- int num = 8;
- num /= 2;
- printf("%d\n", num);
-}
-//---modulas equal to---
-#include <stdio.h>
-int main()
-{
- int num = 8;
- num %= 3;
- //num = (num%2);
- printf("%d\n", num);
+ 
+ //---plus equal to---
+ int num1 = 5;
+ num1 += 1;  //num1 = (num1 + 1);
+ printf("%d\n", num1);
+ 
+ //---minus equal to---
+  int num2 = 5;
+ num2 -= 1;   //num2 = (num2 - 1);
+ printf("%d\n", num2);
+ 
+ //---multiply equal to---
+  int num3 = 5;
+ num3 *= 2;  //num3 = (num3 * 2);
+ printf("%d\n", num3);
+ 
+ //---divide equal to---
+  int num4 = 8;
+ num4 /= 2;    //num4 = (num4 / 2);
+ printf("%d\n", num4);
+ 
+ //---modulas equal to---
+  int num5 = 8;
+ num5 %= 3;     //num5 = (num5 % 2);
+ printf("%d\n", num5);
 }
