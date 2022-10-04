3ac58c47a367704a64e10a2c3337c72063fdd1f0
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2022-10-04 21:59:39 +0600

    Create for_loop_in_c_part_1.c

---
+++ for_loop_in_c_part_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+ /*for(initialization; condition; updateStatement(increment/decrement))
+ for(int i=1; 6<=5; i++)
+ {
+  printf("Hello World!\n");
+  i++ = i+1;
+  
+ }*/
+ for(int i=1; i<=5; i++)
+ {
+  printf("Hello World!\n");
+ }
+}
