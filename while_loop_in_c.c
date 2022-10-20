0e43daebb19a0afbd9e9fb6bf93f9b453b62ad2f
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2022-10-20 21:17:14 +0600

    Create while_loop_in_c.c

---
+++ while_loop_in_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+ int i=1;   //initialization;
+
+ while(i<=10)     //condition;
+ {
+  printf("While Loop _%d\n", i);    //loop body;
+
+  //i=i+1
+  i++; //increment/decrement;
+ }
+}
